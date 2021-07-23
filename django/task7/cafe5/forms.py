43236f6e3ee598fb92a773f1c3947d1124e3946a
komeil13 <komeili.hadi@gmail.com>
2021-07-23 13:43:28 +0430

    task 7 django - forms

---
+++ django/task7/cafe5/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+from django.core.exceptions import ValidationError
+from .models import Category
+
+
+def menu_item_name_validator(name: str):
+    if not name.istitle():
+        raise ValidationError('menu item name must be title!')
+
+
+def menu_item_discount_validator(num: int):
+    if num < 0:
+        raise ValidationError('discount must be 0 or greater!')
+
+
+def menu_item_price_validator(value: int):
+    if value <= 0:
+        raise ValidationError('price must be positive!')
+
+
+class MenuItemForm(forms.Form):
+    menu_item_name = forms.CharField(max_length=50, validators=[menu_item_name_validator], label='Item Name')
+    discount = forms.IntegerField(validators=[menu_item_discount_validator], label='Item Discount')
+    price = forms.FloatField(validators=[menu_item_price_validator], label='Item Price')
+    image = forms.FileField(required=False, label='Item Image')
+    category = forms.ModelChoiceField(queryset=Category.objects.all())
